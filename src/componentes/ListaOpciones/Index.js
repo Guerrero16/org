659b239587e00af45c79162200d09e506a581598
Guerrero16 <rjt1608@gmail.com>
2023-08-28 12:23:57 -0500

    correccion error en listaOpciones

--- src/componentes/ListaOpciones/Index.js
+++ src/componentes/ListaOpciones/Index.js
@@ -15,7 +15,7 @@ const ListaOpciones = (props) => {
         <label htmlFor="">Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
             <option value="" disabled defaultValue="" hidden>Selecionar equipo</option>
-            {props.equipos.map((equipo, index) => <option key={index}>{equipo}</option>)}
+            {props.equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
         </select>
     </div>
 }
